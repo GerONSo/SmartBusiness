87ca1bc75f39731660163217127cebf479335324
Oper80 <oper_80@mail.ru>
2021-02-04 10:50:06 +0200

    init

---
+++ app/src/main/java/dev/geronso/smartbusiness/MenuActivity.kt
@@ -0,0 +1,13 @@
+package dev.geronso.smartbusiness
+
+import android.hardware.biometrics.BiometricPrompt
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+    }
+}
