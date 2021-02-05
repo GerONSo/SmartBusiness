4e4a30b002d53da24072f17994c79f396929b6a3
Yemifino <steepansteepan@gmail.com>
2021-02-05 14:34:48 +0300

    Добавленно немного графики

--- app/src/main/java/dev/geronso/smartbusiness/activities/LoginActivity.kt
+++ app/src/main/java/dev/geronso/smartbusiness/activities/LoginActivity.kt
@@ -2,6 +2,7 @@ package dev.geronso.smartbusiness.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
         val regButton = findViewById<TextView>(R.id.btn_register)
         val loginEditText = findViewById<EditText>(R.id.et_login)
         val passwordEditText = findViewById<EditText>(R.id.et_password)
